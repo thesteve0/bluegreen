4b6f8196e9aaabd2d03c64aa7ee3466ebb54aa12
Steven Pousty <spousty@redhat.com>
2016-05-05 18:36:44 -0700

    color

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import socket
 def index():
     host = socket.gethostname()
     # Green - #008000
-    response_string = "<html> <style> body {background-color: #0000FF;} </style> <body> <h1> <font color="white"> hello blue on: " + str(host)+ " </font> </h1> </body> </html>"
+    response_string = "<html> <style> body {background-color: #0000FF;} </style> <body> <h1> <font color='white'> hello blue on: " + str(host)+ " </font> </h1> </body> </html>"
     return response_string
 
 
