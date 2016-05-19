74a5261c81f8423a0afbc4966207973dd590d748
Steven Pousty <spousty@redhat.com>
2016-05-19 09:15:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ from random_words import RandomWords
 def index():
     host = socket.gethostname()
     # Green - #008000
-    response_string = "<html> <style> body {background-color: #008000;} </style> <body> <h1> <font color='white'> Hello from  green on: " + str(host)+ " </font> </h1> </body> </html>"
+    # Blue - #0000FF
+    response_string = "<html> <style> body {background-color: #0000FF;} </style> <body> <h1> <font color='white'> Hello from  Blue on: " + str(host)+ " </font> </h1> </body> </html>"
     return response_string
 
 # since this is a read only talk to the replicas
